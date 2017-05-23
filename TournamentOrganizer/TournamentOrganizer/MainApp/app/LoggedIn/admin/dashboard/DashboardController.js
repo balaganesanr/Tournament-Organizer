bdca2048bc77d0296e387f092ad86794d7801fa4
balaganesanr <balaganesan.r@live.com>
2017-05-23 22:49:57 +0530

    Merge

---
+++ TournamentOrganizer/TournamentOrganizer/MainApp/app/LoggedIn/admin/dashboard/DashboardController.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../../scripts/angular/ngFormFixes.directive/ngFormFixes.directive.js" />
+define(['angular'], function (angular) {
+
+    var tournamentOrganiser = angular
+         .module('tournamentOrganiser')
+
+
+
+    tournamentOrganiser.registerController('dashboardController',
+     ['dashboardService', '$scope', '$http', '$window', '$timeout',
+         '$filter', 'Enum', '$q', '$http', '$ocLazyLoad', 'authService'
+         , '$injector',
+     DashboardController]);
+
+
+
+    function DashboardController(dashboardService, $scope, $http, $window, $timeout, $filter, Enum, $q, $http, $ocLazyLoad, authService, $injector) {
+        var vm = this;
+
+        return vm;
+    }
+});
---
+++ TournamentOrganizer/TournamentOrganizer/MainApp/app/LoggedIn/admin/dashboard/DashboardService.js
@@ -0,0 +1,11 @@
+﻿define(['angular'], function (angular) {
+    angular.module('tournamentOrganiser')
+        .$register
+        .factory('dashboardService',
+        ['$http', 'appServices', 'authService', 'Enum', '$sce', '$q',
+            function ($http, appServices, authService, Enum, $sce, $q) {
+                var dashboardService = {};
+                
+                return dashboardService;
+            }]);
+});
